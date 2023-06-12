13d302f47ed080824145cb4282fe17f763ba7294
Chris Vasselli <clindsay@gmail.com>
2023-06-12 10:13:44 +0900

    Additional unlocalized string.

--- Sources/SwifterSwift/UIKit/UIViewControllerExtensions.swift
+++ Sources/SwifterSwift/UIKit/UIViewControllerExtensions.swift
@@ -60,7 +60,7 @@ public extension UIViewController {
     /// - Parameters:
     ///   - title: title of the alert.
     ///   - message: message/body of the alert.
-    ///   - buttonTitles: (Optional)list of button titles for the alert. Default button i.e "OK" will be shown if this parameter is nil.
+    ///   - buttonTitles: list of button titles for the alert.
     ///   - highlightedButtonIndex: (Optional) index of the button from buttonTitles that should be highlighted. If this parameter is nil no button will be highlighted.
     ///   - completion: (Optional) completion block to be invoked when any one of the buttons is tapped. It passes the index of the tapped button as an argument.
     /// - Returns: UIAlertController object (discardable).
@@ -68,17 +68,13 @@ public extension UIViewController {
     func showAlert(
         title: String?,
         message: String?,
-        buttonTitles: [String]? = nil,
+        buttonTitles: [String],
         highlightedButtonIndex: Int? = nil,
         completion: ((Int) -> Void)? = nil) -> UIAlertController {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        var allButtons = buttonTitles ?? [String]()
-        if allButtons.count == 0 {
-            allButtons.append("OK")
-        }
 
-        for index in 0..<allButtons.count {
-            let buttonTitle = allButtons[index]
+        for index in 0..<buttonTitles.count {
+            let buttonTitle = buttonTitles[index]
             let action = UIAlertAction(title: buttonTitle, style: .default, handler: { _ in
                 completion?(index)
             })
